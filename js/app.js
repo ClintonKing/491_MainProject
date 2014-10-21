15df0b4a5451aa6d63116a87915757d27a8c1afb
ClintonKing <kingcj@live.unc.edu>
2014-10-21 10:26:35 -0400

    BIG UPDATE 10/21

--- js/app.js
+++ js/app.js
@@ -2,13 +2,51 @@ document.addEventListener("deviceReady", onDeviceReady, false);
 
 //contains code for camera
 
+$( document ).ready(function() {
+    console.log( "ready!" );
+    
+});
+
+
+
+function updateFlyer(){
+      var flyerListRef = new Firebase('https://ezflyer.firebaseio.com//flyers')
+    
+    var flyName = $('#flyNameInput').val();
+    var flyDesc = $('#flyDescInput').val();
+    var flyDate = $('#flyDateInput').val();
+    var flyTime = $('#flyTimeInput').val();
+    var flyPlace = $('#flyPlaceInput').val();
+    var flyImage = $('#flyImageInput').val();
+    
+    //var newFlyerRef = flyerListRef.child(flyName);
+    flyerListRef.push({ name:flyName, desc:flyDesc, date:flyDate, time:flyTime, location:flyPlace, image:flyImage });
+    console.log(flyName);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 function capturePhoto(){
     navigator.camera.getPicture(uploadPhoto,null,{sourceType:1,quality:60});
 }
 
 function uploadPhoto(data){
-    
-    
+
+
     cameraPic.src = data;
     navigator.notification.alert(
         'Your photo has been uploaded', //message
@@ -19,7 +57,7 @@ function uploadPhoto(data){
 }
 
 function okay(){
-    
+
 }
 
 
