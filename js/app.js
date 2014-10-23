43437de2ea96f7e8a9cb766016b1b07137806a3b
ClintonKing <kingcj@live.unc.edu>
2014-10-23 08:25:39 -0400

    Beta Update

--- js/app.js
+++ js/app.js
@@ -1,10 +1,10 @@
-document.addEventListener("deviceReady", onDeviceReady, false);
+//document.addEventListener("deviceReady", onDeviceReady, false);
 
 //contains code for camera
 
 $( document ).ready(function() {
     console.log( "ready!" );
-    
+    loadFlyer();
 });
 
 
@@ -24,6 +24,48 @@ function updateFlyer(){
     console.log(flyName);
     
     document.getElementById("newFlyerForm").reset();
+       
+};
+
+function loadFlyer(){
+    var flyerListRef = new Firebase('https://ezflyer.firebaseio.com//flyers')
+    
+    var flyersToDisplay = 8;
+    var flyerListView = flyerListRef.limit(flyersToDisplay);
+    
+    flyerListRef.once('value', function(dataSnapshot){
+        var listItems = "";
+        // store dataSnapshot for use in examples below
+        flyerListView = dataSnapshot.val();
+        console.log(flyerListView);
+        
+        $.each(flyerListView, function(key, val){
+            var flyer = [];
+            
+            console.log('Key: ' + key + ' Val: ' + val)
+            flyer.push(key);
+            $.each(val, function(key, val){
+                flyer.push(val);
+            });
+            
+            listItems += '<div class="mason"><a href="#" data-reveal-id="' + flyer[5] + '"><img src="' + flyer[3] + '"></a></div>';
+            listItems += '<div id="' + flyer[5] + '" class="reveal-modal" data-reveal>';
+            listItems += '<h2>' + flyer[5] + '</h2>';
+            listItems += '<p>' + flyer[2] + '</p>';
+            listItems += '<p><b>Date: </b>' + flyer[1] + '</p>';
+            listItems += '<p><b>Time: </b>' + flyer[6] + '</p>';
+            listItems += '<p><b>Location: </b>' + flyer[4] + '</p>';
+            listItems += '<a href="index.html">Close</a>';
+            listItems += '</div>'
+            //re-implement class="close-reveal-modal" later to last link.
+        });
+        
+        $("#masonBox").html(listItems);
+       
+        
+    });
+    
+    
     
 }
 
@@ -38,36 +80,3 @@ function updateFlyer(){
 
 
 
-
-
-
-
-
-function capturePhoto(){
-    navigator.camera.getPicture(uploadPhoto,null,{sourceType:1,quality:60});
-}
-
-function uploadPhoto(data){
-
-
-    cameraPic.src = data;
-    navigator.notification.alert(
-        'Your photo has been uploaded', //message
-        okay,  //callback
-        'Photo Uploaded', //title
-        'Ok'  //button name
-    );
-}
-
-function okay(){
-
-}
-
-
-//maps stuff
-
-function onDeviceReady(){
-    console.log("onDeviceReady()");
-    navigator.geolocation.getCurrentPosition(generateMap, onError);
-    console.log(position);
-}
