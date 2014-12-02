7f3b728587eb487d8a2c7a3116d2f4787910383e
ClintonKing <kingcj@live.unc.edu>
2014-12-02 09:55:14 -0500

    Close

--- js/app.js
+++ js/app.js
@@ -11,43 +11,43 @@ $( document ).ready(function() {
 
 function updateFlyer(){
       var flyerListRef = new Firebase('https://ezflyer.firebaseio.com//flyers')
-    
+
     var flyName = $('#flyNameInput').val();
     var flyDesc = $('#flyDescInput').val();
     var flyDate = $('#flyDateInput').val();
     var flyTime = $('#flyTimeInput').val();
     var flyPlace = $('#flyPlaceInput').val();
     var flyImage = $('#flyImageInput').val();
-    
+
     //var newFlyerRef = flyerListRef.child(flyName);
     flyerListRef.push({ name:flyName, desc:flyDesc, date:flyDate, time:flyTime, location:flyPlace, image:flyImage });
     console.log(flyName);
-    
+
     document.getElementById("newFlyerForm").reset();
-       
+
 };
 
 function loadFlyer(){
     var flyerListRef = new Firebase('https://ezflyer.firebaseio.com//flyers')
-    
-    var flyersToDisplay = 8;
+
+    var flyersToDisplay = 12;
     var flyerListView = flyerListRef.limit(flyersToDisplay);
-    
+
     flyerListRef.once('value', function(dataSnapshot){
         var listItems = "";
         // store dataSnapshot for use in examples below
         flyerListView = dataSnapshot.val();
         console.log(flyerListView);
-        
+
         $.each(flyerListView, function(key, val){
             var flyer = [];
-            
+
             console.log('Key: ' + key + ' Val: ' + val)
             flyer.push(key);
             $.each(val, function(key, val){
                 flyer.push(val);
             });
-            
+
             listItems += '<div class="mason"><a href="#" data-reveal-id="' + flyer[5] + '"><img src="' + flyer[3] + '"></a></div>';
             listItems += '<div id="' + flyer[5] + '" class="reveal-modal" data-reveal>';
             listItems += '<h2>' + flyer[5] + '</h2>';
@@ -59,24 +59,14 @@ function loadFlyer(){
             listItems += '</div>'
             //re-implement class="close-reveal-modal" later to last link.
         });
-        
+
         $("#masonBox").html(listItems);
-       
-        
+
+
     });
-    
-    
-    
+
 }
 
-
-
-
-
-
-
-
-
-
-
-
+function vexTest(){
+  vex.dialog.alert('<h1>This is only a test</h1>');
+}
